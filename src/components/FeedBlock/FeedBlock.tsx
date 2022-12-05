9e0afadfbdba6bab140853def8679d36946a75fc
Alesha Tikhonov <zukko961@gmail.com>
2022-12-05 18:53:18 +0300

    fix popup

--- src/components/FeedBlock/FeedBlock.tsx
+++ src/components/FeedBlock/FeedBlock.tsx
@@ -51,7 +51,7 @@ const FeedBock:React.FC<FeedBockProps> = () => {
     <div className="bg-[#fff] mt-7 rounded-md px-4 py-3">
         <div className="flex border-style pb-4">
             <img className="links-style-right mr-2 w-11 h-11 text-xs" src={user.photoURL} alt="user's"/>
-            <div onClick={()=>setopen(true)} className="make-tweet">What's new, {user.displayName}?</div>
+            <div onClick={()=>setopen(true)} className="make-tweet sm:text-[15px]">What's new, {user.displayName}?</div>
         </div>
         <div className="mt-3">
             <ul className="flex justify-between w-[100%]">
@@ -62,7 +62,7 @@ const FeedBock:React.FC<FeedBockProps> = () => {
         </div>
         {/* popup make tweet */}
     <div onClick={()=>setopen(false)} className={`${open ? "opacity-[1] pointer-events-auto" : "opacity-0 pointer-events-none"} fixed top-0 left-0 flex items-center justify-center w-[100vw] h-[100vh] bg-black-rgba transition-all z-30`}>
-        <div onClick={(e)=>{e.stopPropagation()}} className="w-[36%] bg-[#fff] shadow-xl rounded-md max-w-[480px]">
+        <div onClick={(e)=>{e.stopPropagation()}} className="w-[36%] lg:w-auto bg-[#fff] shadow-xl rounded-md max-w-[480px]">
             <div className="flex items-center justify-center border-style py-3 relative">
                 <h5 className="text-3xl font-medium">Create a post</h5>
                 <button onClick={()=>setopen(false)} className="common-hover absolute right-2 top-2 bg-[#e4e6eb] rounded-full p-1"><IoMdClose className="text-3xl text-[#606770]"/></button> {/* close button */}
@@ -76,7 +76,7 @@ const FeedBock:React.FC<FeedBockProps> = () => {
                     </div>
                 </div>
                 <textarea value={tweet} onChange={(e)=>{setTweet(e.target.value)}} className="py-10 outline-0 h-auto w-[100%]" name="" id="" placeholder={`What's new, ${user.displayName}?`}/>
-                <input value={img} onChange={(e)=>{setImg(e.target.value)}} type="text" placeholder="URl img"/>
+                <input value={img} onChange={(e)=>{setImg(e.target.value)}} type="text" placeholder="URL img"/>
                 <div className="mt-4">
                     <button disabled={tweet.length === 0 && true} onClick={()=>setopen(false)} className={`${tweet.length === 0 && 'cursor-not-allowed bg-[grey]'} flex items-center justify-center w-[100%] bg-[#1b74e4] rounded-md py-2 text-center text-[#fff]`}>tweet</button>
                 </div>
